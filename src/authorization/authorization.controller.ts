1ce00da9d3aa3d24a26543d2eacd66c13a54679a
Andrey <brin3oll@gmail.com>
2022-10-26 20:25:41 +0300

    fix: authorization royte fix

--- src/authorization/authorization.controller.ts
+++ src/authorization/authorization.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Get, Req, UnauthorizedException } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { alchemy } from 'src/alchemy.adapter';
+import { JwtAuthGuard } from 'src/authentication/jwt.guard';
 import { UserSessionService } from 'src/user-session/user-session.service';
 
 @Controller('authorization')
 export class AuthorizationController {
   constructor(private userSessionService: UserSessionService) {}
+
+  @UseGuards(JwtAuthGuard)
   @Get()
-  async checkNFTs(@Req() req: Request) {
-    const token = req.cookies['auth-token'];
-    if (!token) throw new UnauthorizedException();
+  async checkNFTs(@Request() req) {
+    const token = req.user.uri;
     const wallet = await this.userSessionService.getWalletOf(token);
+    console.log('GET /authorization - check NFT of wallet [' + wallet + ']');
+
     return {
       ownsNFT: await alchemy.nft.checkNftOwnership(wallet, [
         '0x89de3d3cbceb94734393cc857886aad4da6bc111',
